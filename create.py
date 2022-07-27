0f1a52513e56601653f642a84d8f8506e5558201
Ubuntu <waqas@finalproject.vuozfm4v5s1eplgyxcmi0n4inh.zx.internal.cloudapp.net>
2022-07-27 14:45:12 +0000

    Imported new html from Bootstrap, CRUD functionality is complete

---
+++ create.py
@@ -0,0 +1,21 @@
+from app import db, Data, Car
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+person1 = Data(name='Waqas Arqum' email='waqy_arqum@hotmail.co.uk')
+db.session.add(person1)
+db.session.commit()
+
+car1 = Car(reg="12345", databr_id=person1)
+db.session.add(car1)
+db.session.commit()
+car2 = Car(reg="123456", databr_id=person1)
+db.session.add(car2)
+db.session.commit()
+
+print(person1.car)
+
+print(car1.databr.name + ', ' + car1.databr.email)
